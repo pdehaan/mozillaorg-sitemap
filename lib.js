ac44c6f15d0b82885cebbe50e0b9b37411bad382
Peter deHaan <peter@deseloper.com>
2019-08-15 12:15:33 -0700

    Minorly refactor lib

--- lib.js
+++ lib.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const check = require("check-broken-links");
 
 const MAX_LINKS = process.env.MAX_LINKS || 5000;
-const ORIGIN = "https://mozilla.org";
+const ORIGIN = process.env.ORIGIN || "https://mozilla.org";
 
 module.exports = {
   checkLinks,
@@ -14,19 +14,21 @@ async function localeSitemap(locale = "en-US", jsonSitemapURI = "sitemap.json") 
   const sitemapUri = new URL(jsonSitemapURI, ORIGIN).href;
   const res = await axios.get(sitemapUri);
 
-  return {
-    locale,
-    origin: ORIGIN,
-    sitemap: _localeSitemap(res.data, locale, ORIGIN)
-  };
+  return _localeSitemap(res.data, locale);
 }
 
-function _localeSitemap(sitemap, locale, origin) {
-  return Object.entries(sitemap)
+function _localeSitemap(sitemap, locale, origin = ORIGIN) {
+  const links = Object.entries(sitemap)
     .filter(([, locales]) => locales.includes(locale))
     .map(([page]) => new URL(locale + page, origin).href)
     .sort()
     .slice(0, MAX_LINKS);
+
+  return {
+    locale,
+    origin,
+    sitemap: links
+  };
 }
 
 async function checkLinks(sitemap = [], origin = ORIGIN) {
